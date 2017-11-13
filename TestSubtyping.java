bcce7de3b50f72448b66c06401884f3455ad0f87
jacobsqy <jacobsqy@zenbook.it>
2017-11-13 15:39:43 +0100

    imports

--- TestSubtyping.java
+++ TestSubtyping.java
@@ -1,8 +1,10 @@
+import java.util.List;
+
 public class TestSubtyping {
   Polygon[] polArray = new Polygon[3];
   Triangle[] triArray = new Triangle[3];
-  List<Polygon> polList = new List<Polygon>(3);
-  List<Triangle> triList = new List<Triangle>(3);
+  List<Polygon> polList;
+  List<Triangle> triList;
 
   
 }
