e286a6a37b9e3cab0aab19e40b8f8098402be647
jacobsqy <jacobsqy@zenbook.it>
2017-11-13 15:55:28 +0100

    punkt 1

--- TestSubtyping.java
+++ TestSubtyping.java
@@ -1,10 +1,29 @@
 import java.util.List;
 
 public class TestSubtyping {
-  Polygon[] polArray = new Polygon[3];
-  Triangle[] triArray = new Triangle[3];
+  Polygon[] polArray;
+  Triangle[] triArray;
   List<Polygon> polList;
   List<Triangle> triList;
 
-  
+  public void main(String[] args) {
+    polArray = new Polygon[3];
+    triArray = new Triangle[3];
+
+    polArray[0] = new Triangle(1, 1);
+    // polArray[1] = new Object();
+
+    // triArray[0] = new Object();
+    triArray[1] = new Triangle(1,1);
+    // triArray[2] = new Polygon();
+
+    polList.add(new Triangle(1,1));
+    // polList.add(new Polygon());
+    // polList.add(new Object());
+
+    triList.add(new Triangle(1,1));
+    // triList.add(new Polygon());
+    // triList.add(new Object());
+  }
+
 }
